6db4eb821ccc929496502cd6a0e874861ef54d5b
ShiekhUzi <90346954+ShiekhUzi@users.noreply.github.com>
2022-11-02 23:30:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// function fadeout() {
+
+//     var tnflogo = document.getElementsByClassName("tnfnavlogo")
+//     tnflogo.style.animationName= "unfade";
+//     tnflogo.style.animationDuration= "0.50s";
+//     tnflogo.style.animationFillMode= 'alternate';
+//     tnflogo.style.opacity= "1";
+// }
